1d94737fd65ea93161410d73f2fa917144e6d672
CassianoNeves <cassianodasnevesc10@gmail.com>
2016-06-11 11:51:29 -0300

    feat(paciente)

---
+++ projeto_MVC/Dominio/Paciente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace projeto_MVC.Dominio
+{
+    public class Paciente
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public String Nome { get; set; }
+
+        [Required]
+        public String CPF { get; set; }
+
+        [Required]
+        public String Email { get; set; }
+    }
+}
--- projeto_MVC/Dominio/contexto.cs
+++ projeto_MVC/Dominio/contexto.cs
@@ -17,5 +17,6 @@ namespace projeto_MVC.Dominio
         public DbSet<Especialidade> Especialidade { get; set; }
         public DbSet<Medico> Medico { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Paciente> Paciente { get; set; }
     }
 }
