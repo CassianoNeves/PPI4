cd6d87f666772af560107cf469335fdaf86db00a
rca_diego_knorst <diego@M253.intranet.rca.net>
2016-06-26 22:39:27 -0300

    Ajustes gerais

--- projeto_MVC/Scripts/main.js
+++ projeto_MVC/Scripts/main.js
@@ -1,1 +1,24 @@
-    
+$(document).ready(function () {
+
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": false,
+        "positionClass": "toast-top-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "200",
+        "hideDuration": "1000",
+        "timeOut": "2000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    $('.cpf').mask('000.000.000-00');
+    $('.crm').mask('0000000');
+
+});
